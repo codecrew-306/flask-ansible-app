5d6684f6c171ff263ab57ec885251932e5bb2ad7
codecrew-306 <codecrew306@gmail.com>
2026-02-21 12:10:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Ansible Deployment Success ...."
+    return "Ansible Deployment Success Aslah...."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
